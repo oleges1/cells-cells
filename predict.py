32dd7f41b0b9f018eff2a23d50c6e92f6c8b6c8e
Oleg Desheulin <ya.hef@yandex.ru>
2019-08-28 10:21:50 +0300

    add predict

--- predict.py
+++ predict.py
@@ -10,6 +10,7 @@ from dataloader import *
 from models import *
 from torch.nn.parallel.data_parallel import data_parallel
 from torch.utils.data.dataloader import DataLoader
+import numpy as np
 
 def predict_model(config):
     test_dataset = ImagesDS(config.test.csv_file, config.test.img_dir, mode='test')
@@ -28,9 +29,51 @@ def predict_model(config):
                 result[name] = int(out.argmax().cpu().numpy())
     return result
 
+def select_plate_group(idx, pp_mult, all_test_exp, test_csv, exp_to_group):
+    sub_test = test_csv.loc[test_csv.experiment == all_test_exp[idx],:]
+    assert len(pp_mult) == len(sub_test)
+    mask = np.repeat(plate_groups[np.newaxis, :, exp_to_group[idx]], len(pp_mult), axis=0) != \
+           np.repeat(sub_test.plate.values[:, np.newaxis], 1108, axis=1)
+    pp_mult[mask] = 0
+    return pp_mult
+
 def leak_postprocess(config, predicts):
+    train_csv = pd.read_csv(config.train.csv_file)
     test_csv = pd.read_csv(config.test.csv_file)
-    # TODO: dodododododo 
+
+    plate_groups = np.zeros((1108,4), int)
+    for sirna in range(1108):
+        grp = train_csv.loc[train_csv.sirna==sirna,:].plate.value_counts().index.values
+        assert len(grp) == 3
+        plate_groups[sirna,0:3] = grp
+        plate_groups[sirna,3] = 10 - grp.sum()
+
+    all_test_exp = test_csv.experiment.unique()
+    group_plate_probs = np.zeros((len(all_test_exp),4))
+    for idx in range(len(all_test_exp)):
+        preds = sub.loc[test_csv.experiment == all_test_exp[idx],'sirna'].values
+        pp_mult = np.zeros((len(preds),1108))
+        pp_mult[range(len(preds)),preds] = 1
+
+        sub_test = test_csv.loc[test_csv.experiment == all_test_exp[idx],:]
+        assert len(pp_mult) == len(sub_test)
+
+        for j in range(4):
+            mask = np.repeat(plate_groups[np.newaxis, :, j], len(pp_mult), axis=0) == \
+                   np.repeat(sub_test.plate.values[:, np.newaxis], 1108, axis=1)
+
+            group_plate_probs[idx,j] = np.array(pp_mult)[mask].sum()/len(pp_mult)
+    exp_to_group = group_plate_probs.argmax(1)
+
+    result = np.zeros((len(test_csv), 1))
+    for idx in range(len(all_test_exp)):
+        #print('Experiment', idx)
+        indices = (test_csv.experiment == all_test_exp[idx])
+        preds = predicts[indices,:].copy()
+        preds = select_plate_group(idx, preds)
+        result[indices] = preds.argmax(1)
+    return result
+
 
 def save_csv(config, predicts):
     submission = pd.read_csv(config.test.csv_file)
